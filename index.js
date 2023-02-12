47f849ab85236b235c108d199f1ee9faf838cf64
Yizhen Chen <58789552+jimchen123123321@users.noreply.github.com>
2023-02-11 19:13:28 -0500

    1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { createApp, Vue } from "https://mavue.mavo.io/mavue.js";
 
 let app = createApp({
+	
+	
 	data: {
 		tasks: [
 			{
@@ -26,7 +28,9 @@ let app = createApp({
 		clearCompleted () {
 			this.tasks = this.tasks.filter(task => !task.done);
 		},
-
+		setCheck(i){
+			this.tasks[i].done = !this.tasks[i].done
+		},
 		setActive(i) {
 			for (let task of this.tasks) {
 				task.active = false;
